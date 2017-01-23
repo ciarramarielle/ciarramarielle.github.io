352d9d045018e7b6993cce89fe6ebffd9bc9a9b3
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-01-23 11:50:53 -0800

    Trying another CORS workaround...

--- src/server/express.js
+++ src/server/express.js
@@ -13,25 +13,29 @@ module.exports = function(app, config) {
 
     // CORS Stuff..
     // Add headers
-    app.use(function (req, res, next) {
-
-        // Website you wish to allow to connect
-        res.setHeader('Access-Control-Allow-Origin', '*');
-
-        // Request methods you wish to allow
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-        // Request headers you wish to allow
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-        // Set to true if you need the website to include cookies in the requests sent
-        // to the API (e.g. in case you use sessions)
-        res.setHeader('Access-Control-Allow-Credentials', true);
-
-        // Pass to next layer of middleware
-        next();
+    // app.use(function (req, res, next) {
+    //
+    //     // Website you wish to allow to connect
+    //     res.setHeader('Access-Control-Allow-Origin', '*');
+    //
+    //     // Request methods you wish to allow
+    //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //
+    //     // Request headers you wish to allow
+    //     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //
+    //     // Set to true if you need the website to include cookies in the requests sent
+    //     // to the API (e.g. in case you use sessions)
+    //     res.setHeader('Access-Control-Allow-Credentials', true);
+    //
+    //     // Pass to next layer of middleware
+    //     next();
+    // });
+    app.options('/other', function(req, res){
+        console.log("writing headers only");
+        res.header("Access-Control-Allow-Origin", "https://powerful-harbor-68074.herokuapp.com/");
+        res.end('');
     });
-
 
     // Static routing for anything inside <root=mean_start>/src/client into /
     app.use(express.static(config.rootPath + "src/client"));
